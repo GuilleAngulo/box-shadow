b7b9cd9f798bbeabd8ad66226c2e0b2e1b0d351d
Guillermo Angulo <anyermo@gmail.com>
2021-03-18 17:00:21 -0300

    Fix typecheck error

--- src/components/NumberField/stories.tsx
+++ src/components/NumberField/stories.tsx
@@ -1,10 +1,10 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import { Email } from '@styled-icons/material-outlined'
-import TextField, { TextFieldProps } from '.'
+import NumberField, { NumberFieldProps } from '.'
 
 export default {
-  title: 'Form/TextField',
-  component: TextField,
+  title: 'Form/NumberField',
+  component: NumberField,
   args: {
     label: 'E-mail',
     name: 'email',
@@ -22,15 +22,15 @@ export default {
   }
 } as Meta
 
-export const Default: Story<TextFieldProps> = (args) => (
+export const Default: Story<NumberFieldProps> = (args) => (
   <div style={{ maxWidth: 300, padding: 15 }}>
-    <TextField {...args} />
+    <NumberField {...args} />
   </div>
 )
 
-export const withError: Story<TextFieldProps> = (args) => (
+export const withError: Story<NumberFieldProps> = (args) => (
   <div style={{ maxWidth: 300, padding: 15 }}>
-    <TextField {...args} />
+    <NumberField {...args} />
   </div>
 )
 
@@ -38,9 +38,9 @@ withError.args = {
   error: 'An error ocurred.'
 }
 
-export const withLoading: Story<TextFieldProps> = (args) => (
+export const withLoading: Story<NumberFieldProps> = (args) => (
   <div style={{ maxWidth: 300, padding: 15 }}>
-    <TextField {...args} />
+    <NumberField {...args} />
   </div>
 )
 
