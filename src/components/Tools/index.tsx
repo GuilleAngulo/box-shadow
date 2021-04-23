b89e013c12b60c95ec749d4e936cc18ee326a052
Guillermo Angulo <anyermo@gmail.com>
2021-04-23 19:19:31 -0300

    Refactor Tools

--- src/components/Tools/index.tsx
+++ src/components/Tools/index.tsx
@@ -1,32 +1,114 @@
 import { useBoxShadow } from 'hooks/use-box-shadow'
-import ShadowTool from 'components/ShadowTool'
+import Checkbox from 'components/Checkbox'
+import Color from 'components/Color'
+import InputRange from 'components/InputRange'
+import {
+  HORIZONTAL_OFFSET_RANGES,
+  VERTICAL_OFFSET_RANGES,
+  BLUR_RADIUS_RANGES,
+  SPREAD_RADIUS_RANGES
+} from 'utils/shadow'
+
+import {
+  MoveHorizontal,
+  MoveVertical,
+  Move
+} from '@styled-icons/boxicons-regular'
+import { BlurOn } from '@styled-icons/material-outlined'
+import { ColorFill } from '@styled-icons/boxicons-regular'
+import { Close } from '@styled-icons/material-outlined'
+import * as S from './styles'
 
 import { LayerPlus as AddIcon } from '@styled-icons/boxicons-regular'
 
-import * as S from './styles'
-import { useMemo } from 'react'
-
 const Tools = () => {
-  const { boxShadow = [], addBoxShadow } = useBoxShadow()
-
-  const range = useMemo(() => {
-    const output = []
-
-    for (let i = 0; i < boxShadow.length; i++) {
-      output.push(i)
-    }
-
-    return output
-  }, [boxShadow])
+  const {
+    boxShadow = [],
+    addBoxShadow,
+    setBoxShadowProperty,
+    removeBoxShadow
+  } = useBoxShadow()
 
   return (
     <S.Wrapper>
       <S.ToolsWrapper>
-        <>
-          {range.map((index) => (
-            <ShadowTool key={index} index={index} />
-          ))}
-        </>
+        {boxShadow.map((item, index) => {
+          const horizontalOffset = boxShadow[index]?.horizontalOffset
+          const verticalOffset = boxShadow[index]?.verticalOffset
+          const blurRadius = boxShadow[index]?.blurRadius
+          const spreadRadius = boxShadow[index]?.spreadRadius
+          const inset = boxShadow[index]?.inset
+          return (
+            <S.Item
+              key={index}
+              aria-label={`Tool for Box Shadow Level ${index}`}
+            >
+              <S.Delete
+                aria-label={`Delete Box Shadow Level ${index}`}
+                onClick={() => removeBoxShadow(index)}
+              >
+                <Close width={24} />
+              </S.Delete>
+              <InputRange
+                label="Horizontal Offset"
+                name="horizontal-offset"
+                min={HORIZONTAL_OFFSET_RANGES[0]}
+                max={HORIZONTAL_OFFSET_RANGES[1]}
+                initialValue={horizontalOffset}
+                icon={<MoveHorizontal />}
+                onInput={(value: number) =>
+                  setBoxShadowProperty(index, 'horizontalOffset', value)
+                }
+              />
+              <InputRange
+                label="Vertical Offset"
+                name="vertical-offset"
+                min={VERTICAL_OFFSET_RANGES[0]}
+                max={VERTICAL_OFFSET_RANGES[1]}
+                initialValue={verticalOffset}
+                icon={<MoveVertical />}
+                onInput={(value: number) =>
+                  setBoxShadowProperty(index, 'verticalOffset', value)
+                }
+              />
+              <InputRange
+                label="Blur Radius"
+                name="blur-radius"
+                min={BLUR_RADIUS_RANGES[0]}
+                max={BLUR_RADIUS_RANGES[1]}
+                initialValue={blurRadius}
+                icon={<BlurOn />}
+                onInput={(value: number) =>
+                  setBoxShadowProperty(index, 'blurRadius', value)
+                }
+              />
+              <InputRange
+                label="Spread Radius"
+                name="spread-radius"
+                min={SPREAD_RADIUS_RANGES[0]}
+                max={SPREAD_RADIUS_RANGES[1]}
+                initialValue={spreadRadius}
+                icon={<Move />}
+                onInput={(value: number) =>
+                  setBoxShadowProperty(index, 'spreadRadius', value)
+                }
+              />
+              <Color
+                index={index}
+                label="Color - Opacity"
+                icon={<ColorFill />}
+              />
+              <Checkbox
+                onCheck={() => setBoxShadowProperty(index, 'inset')}
+                isChecked={inset}
+                label="Inset"
+              />
+              <S.Index aria-label={`Box Shadow level ${index + 1}`}>
+                {index + 1}
+              </S.Index>
+            </S.Item>
+          )
+        })}
         <S.Add
           role="button"
           aria-label="create new box shadow"
--- src/components/Tools/styles.ts
+++ src/components/Tools/styles.ts
@@ -1,8 +1,8 @@
+import { darken } from 'polished'
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
 export const Wrapper = styled.div``
-export const Item = styled.div``
 export const Color = styled.div``
 
 export const ToolsWrapper = styled.ul`
@@ -48,5 +48,48 @@ export const Add = styled.li`
         transform: translateY(-0.5rem);
       }
     }
+  `}
+`
+
+export const Item = styled.li`
+  ${({ theme }) => css`
+    list-style: none;
+    position: relative;
+    padding: 0.5rem 1.2rem 1rem 1.2rem;
+    /* height: fit-content; */
+    height: 35rem;
+    background-color: ${theme.colors.card};
+    border-radius: ${theme.border.radius};
+    border: 1px solid ${theme.colors.accent};
+    transition: box-shadow ${theme.transition.default};
+  `}
+`
+
+export const Delete = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+    top: 0;
+    right: 0;
+    cursor: pointer;
+    margin-top: 0.2rem;
+    margin-right: 0.2rem;
+    border-radius: ${theme.border.radius};
+    color: ${theme.colors.secondaryFont};
+
+    &:hover {
+      background: ${darken(0.1, theme.colors.card)};
+      transition: background ${theme.transition.fast};
+    }
+  `}
+`
+
+export const Index = styled.span`
+  ${({ theme }) => css`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding-bottom: 0.6rem;
+    margin-right: 0.8rem;
+    color: ${theme.colors.secondaryFont};
   `}
 `
