d098e3e659dd97e3676189099d7e1d9f45d3dd25
Guillermo Angulo <anyermo@gmail.com>
2021-05-27 18:21:24 -0300

    Fix typecheck error on GalleryItem

--- src/components/GalleryItem/stories.tsx
+++ src/components/GalleryItem/stories.tsx
@@ -1,9 +1,11 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GalleryItem from '.'
+import GalleryItem, { GalleryItemProps } from '.'
 
 export default {
   title: 'GalleryItem',
   component: GalleryItem
 } as Meta
 
-export const Default: Story = () => <GalleryItem />
+export const Default: Story<GalleryItemProps> = (args) => (
+  <GalleryItem {...args} />
+)
